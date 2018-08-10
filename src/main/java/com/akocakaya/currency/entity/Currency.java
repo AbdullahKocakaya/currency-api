c77dd3d2ef078613ef8af592e7fc02f0c31b1623
Abdullah Kocakaya <abdullahkocakaya4@gmail.com>
2018-08-10 16:35:36 +0300

    date added

--- src/main/java/com/akocakaya/currency/entity/Currency.java
+++ src/main/java/com/akocakaya/currency/entity/Currency.java
@@ -3,9 +3,15 @@ package com.akocakaya.currency.entity;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @XmlRootElement(name = "Currency")
 public class Currency {
+
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    Date today = new Date();
 
     @XmlAttribute(name = "CurrencyCode")
     private String currencyCode;
@@ -15,6 +21,9 @@ public class Currency {
 
     @XmlElement(name = "ForexBuying")
     private String value;
+
+    @XmlElement
+    private String date = dateFormat.format(today);
 
     public String getCurrencyCode() {
         return currencyCode;
@@ -26,5 +35,9 @@ public class Currency {
 
     public String getValue() {
         return value;
+    }
+
+    public String getDate() {
+        return date;
     }
 }
