436731a115a4600cea6cb488773320d664a4dcc4
Abdullah Kocakaya <abdullahkocakaya4@gmail.com>
2018-08-10 16:12:05 +0300

    currency controller added for getting all currencies

---
+++ src/main/java/com/akocakaya/currency/control/CurrencyController.java
@@ -0,0 +1,20 @@
+package com.akocakaya.currency.control;
+
+import com.akocakaya.config.CurrencyList;
+import com.akocakaya.currency.entity.Currency;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CurrencyController {
+
+    @Inject
+    CurrencyList currencyList;
+
+    public List<Currency> getCurrencies() {
+        return new ArrayList<>(currencyList.getCurrencyHashMap().values());
+    }
+}
