fcfaff63021a88c3b0ceb882c015cbf956d9a532
Abdullah Kocakaya <abdullahkocakaya4@gmail.com>
2018-09-03 12:16:39 +0300

    weekend bug fixed

--- src/main/java/com/akocakaya/currency/controller/DateFormatter.java
+++ src/main/java/com/akocakaya/currency/controller/DateFormatter.java
@@ -16,18 +16,31 @@ public class DateFormatter {
             date = yesterday();
         }
 
-        try {
-            return new SimpleDateFormat("yyyyMM/ddMMyyyy")
-                        .format(new SimpleDateFormat("yyyy-dd-MM")
-                        .parse(date));
-        } catch (ParseException e) {
-            return e.getMessage();
-        }
+        isDateWeekend(date);
+
+        return new SimpleDateFormat("yyyyMM/ddMMyyyy").format(isDateWeekend(date));
     }
 
     private String yesterday() {
         final Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE, -1);
         return new SimpleDateFormat("yyyy-dd-MM").format(calendar.getTime());
+    }
+
+    private Date isDateWeekend(String date) {
+        Calendar calendar = Calendar.getInstance();
+
+        try {
+            calendar.setTime(new SimpleDateFormat("yyyy-dd-MM").parse(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        while(true) {
+            if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)
+                calendar.add(Calendar.DATE, -1);
+            else break;
+        }
+        return calendar.getTime();
     }
 }
