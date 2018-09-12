b3a53aa4f044f7bb4dcbc02c098766fb8681fe58
Abdullah Kocakaya <abdullahkocakaya4@gmail.com>
2018-09-12 09:44:42 +0300

    access-control-allow-origin added

--- src/main/java/com/akocakaya/currency/boundary/CurrencyController.java
+++ src/main/java/com/akocakaya/currency/boundary/CurrencyController.java
@@ -3,10 +3,7 @@ package com.akocakaya.currency.boundary;
 import com.akocakaya.currency.controller.CurrencyService;
 import com.akocakaya.currency.controller.DateFormatter;
 import com.akocakaya.currency.entity.Currency;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -21,11 +18,13 @@ public class CurrencyController {
     @Inject
     private DateFormatter dateFormatter;
 
+    @CrossOrigin(origins = "*")//Access-Control-Allow-Origin http://127.0.0.1:5500
     @RequestMapping(value = "/getCurrencies/{date}")
     public List<Currency> getCurrencies(@PathVariable String date) {
         return currencyService.getCurrencies(dateFormatter.dateFormatter(date));
     }
 
+    @CrossOrigin(origins = "*")//Access-Control-Allow-Origin http://127.0.0.1:5500
     @RequestMapping(value = "/getCurrency/{date}")
     public Currency getCurrency(@PathVariable String date, @RequestParam(value = "currencyCode") String currencyCode) {
         return currencyService.getCurrency(currencyCode, dateFormatter.dateFormatter(date));
